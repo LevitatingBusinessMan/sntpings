5998f2d8789a1d4888e0769b5cfd0ff0ff0b7c30
LevitatingBusinessMan (Rein Fernhout) <me@levitati.ng>
2024-11-01 05:10:25 +0100

    brrr

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,36 @@
 use std::{any, cell::LazyCell, net::{IpAddr, Ipv6Addr}, path::PathBuf, ptr::addr_of_mut, sync::LazyLock, time::{Duration, Instant}};
 
-use image;
-use clap::{self, Parser};
+use image::{self, GenericImageView, Rgba};
+use clap::Parser;
 use anyhow;
 use pnet::packet::{icmpv6::{echo_request::{EchoRequestPacket, Icmpv6Codes}, Icmpv6Code, Icmpv6Types}, ip::{IpNextHeaderProtocol, IpNextHeaderProtocols::{self, Ipv6}}, util, Packet};
-use rand::random;
+use rand::seq::SliceRandom;
+
+static SNT_ASCII: &'static str = include_str!("snt.asc");
 
 #[derive(clap::Parser)]
 struct Args {
-    #[arg(short, long)]
+    #[arg(short, long, help="image file to ping")]
     file: PathBuf,
-    #[arg(short, long, default_value="1")]
+    #[arg(short, long, default_value="1", help="interval for printing stats")]
     interval: u64,
+    #[arg(short, long, default_value="0", help="x offset")]
+    x: u16,
+    #[arg(short, long, default_value="0", help="y offset")]
+    y: u16, 
+    #[arg(short, long, default_value="false", help="only print once")]
+    once: bool,
+    #[arg(long, requires="scale_y")]
+    scale_x: Option<u32>,
+    #[arg(long, requires="scale_x")]
+    scale_y: Option<u32>,
+    #[arg(long, default_value="true", help="shuffle pixel order")]
+    shuffle: bool,
 }
 
 static DESTADDR: (u16, u16, u16, u16) = (0x2001, 0x610, 0x1908, 0xa000);
 
+#[inline]
 fn craftaddr(x: u16, y: u16, r: u8, g: u8, b: u8) -> IpAddr {
     std::net::IpAddr::V6(Ipv6Addr::new(
         DESTADDR.0,
@@ -36,7 +51,17 @@ fn craftaddr(x: u16, y: u16, r: u8, g: u8, b: u8) -> IpAddr {
 
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
-    let image = image::open(args.file)?;
+    println!("{SNT_ASCII}");
+    let mut image = image::open(args.file)?;
+
+    if args.scale_x.is_some() {
+        println!("Resizing to {} by {}", args.scale_x.unwrap(), args.scale_y.unwrap());
+        image = image.resize_exact(
+            args.scale_x.unwrap(),
+            args.scale_y.unwrap(),
+            image::imageops::FilterType::Gaussian
+        );
+    }
 
     use pnet::transport::TransportChannelType::Layer4;
     use pnet::transport::TransportProtocol::Ipv6;
@@ -56,8 +81,6 @@ fn main() -> anyhow::Result<()> {
     echoreq.set_checksum(checksum);
     let echoreq = echoreq.consume_to_immutable();
 
-    let mut x = 0;
-    let mut y = 0;
     let mut timer = Instant::now();
     let interval = Duration::new(args.interval, 0);
 
@@ -65,28 +88,34 @@ fn main() -> anyhow::Result<()> {
     let mut countpks = 0;
     let mut errors_total = 0;
     let mut errorspks = 0;
-    loop {
-        if let Err(_) = tx.send_to(&echoreq, craftaddr(1420 + x,30 + y,0,0,0xff)) {
-            errors += 1;
-            errorspks += 1;
-        }
-        countpks += 1;
-        count_total += 1;
-        x += 1;
-        if x > 10 {
-            x = 0;
-            y += 1;
-        }
-        if y > 10 {
-            y = 0;
-        }
-        if timer.elapsed() > interval {
-            timer = Instant::now();
-            println!("{countpks}pks {errorspks}eps {count_total} packets {errors_total} errors");
-            errorspks = 0;
-            countpks = 0;
-        }
+
+    let mut pixels: Vec<(u32, u32, Rgba<u8>)> = image.pixels().collect();
+    if args.shuffle {
+        pixels.shuffle(&mut rand::thread_rng());
     }
     
+    loop {
+        for (x, y, Rgba([r, g, b, a])) in &pixels {
+            if *a < 0xff {
+
+            }
+            if let Err(_) = tx.send_to(&echoreq, craftaddr(args.x + *x as u16, args.y + *y as u16 , *r, *g, *b)) {
+                errors_total += 1;
+                errorspks += 1;
+            }
+            countpks += 1;
+            count_total += 1;
+            if timer.elapsed() > interval {
+                timer = Instant::now();
+                println!("{countpks}pks {errorspks}eps ({:02.0}%) {count_total} packets {errors_total} errors", 100.0 * errorspks as f64 / countpks as f64);
+                errorspks = 0;
+                countpks = 0;
+            }
+        }
+        if args.once {
+            break;
+        }
+    }
+
     Ok(())
 }
